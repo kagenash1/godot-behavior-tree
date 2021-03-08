ab8397d28b7f6c16c32a845c3ecb3f22e3a21412
Gabriele Torini <gabriele.torini@outlook.it>
2021-03-08 05:31:14 +0100

    Update bt_sequence.gd

--- bt_sequence.gd
+++ bt_sequence.gd
@@ -1,6 +1,12 @@
 class_name BTSequence, "icons/btsequence.svg" 
 extends BTComposite
 
+# Ticks its children as long as ALL of them are successful.
+# Successful if ALL the children are successful.
+# Fails if ANY of the children fails.
+
+
+
 func _tick(agent: Node, blackboard: Blackboard) -> bool:
 	for c in get_children():
 		var bt_child: BTNode = c
