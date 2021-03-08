aa229ea71e58ae74f333b99f05feb2731ef2d106
Gabriele Torini <gabriele.torini@outlook.it>
2021-03-08 05:20:53 +0100

    Update bt_composite.gd

--- bt_composite.gd
+++ bt_composite.gd
@@ -1,3 +1,10 @@
 class_name BTComposite
 extends BTNode
 
+func _tick(agent: Node, blackboard: Blackboard) -> bool:
+	for c in get_children():
+		var bt_child: BTNode = c
+		var result = bt_child.tick(agent, blackboard)
+    if bt_child.running() and result is GDScriptFunctionState:
+      yield(result, "completed")
+	return succeed()
