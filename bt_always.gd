2f0caf7c6d4d622c81893175de8caa7b0a2f620b
Gabriele Torini <gabriele.torini@outlook.it>
2021-03-08 05:24:02 +0100

    Update bt_always.gd

--- bt_always.gd
+++ bt_always.gd
@@ -1,6 +1,9 @@
 class_name BTAlways 
 extends BTDecorator
 
+# Executes the child and always either succeeds or fails, depending on what you set from the inspector
+
+
 export(String, "fail", "succeed") var always_what
 
 
