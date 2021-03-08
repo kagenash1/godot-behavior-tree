c20dedba11d5dc36c6fc93ec46b9e8b57f60dc8c
Gabriele Torini <gabriele.torini@outlook.it>
2021-03-08 05:30:15 +0100

    Update bt_selector.gd

--- bt_selector.gd
+++ bt_selector.gd
@@ -1,6 +1,11 @@
 class_name BTSelector, "icons/btselector.svg"
 extends BTComposite
 
+# Ticks its children until ANY of them succeeds, thus succeeding.
+# If ALL of the children fails, it fails as well.
+
+
+
 func _tick(agent: Node, blackboard: Blackboard) -> bool:
 	for c in get_children():
 		var bt_child: BTNode = c
