68013a9d3f38bce64149fd1e05a737f4136f0c02
Gabriele Torini <gabriele.torini@outlook.it>
2021-03-08 05:28:17 +0100

    Update bt_parallel.gd

--- bt_parallel.gd
+++ bt_parallel.gd
@@ -1,6 +1,11 @@
 class_name BTParallel, "icons/btparallel.svg"
 extends BTComposite
 
+# Executes each child. Differently from a base BTComposite, it doesn't wait for completion.
+# Useful if you wanna run stuff in... parallel, regardless of the tick result.
+
+
+
 func _tick(agent: Node, blackboard: Blackboard) -> bool:
 	for c in get_children():
 		var bt_child: BTNode = c
