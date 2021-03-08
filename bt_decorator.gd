10e495c300449f22b66a97e4fa4404f572d57ac4
Gabriele Torini <gabriele.torini@outlook.it>
2021-03-08 05:18:04 +0100

    Update bt_decorator.gd

--- bt_decorator.gd
+++ bt_decorator.gd
@@ -5,5 +5,12 @@ onready var bt_child: BTNode = get_child(0) as BTNode
 
 
 
+func _tick(agent: Node, blackboard: Blackboard) -> bool:
+	var result = bt_child.tick(agent, blackboard)
+	if bt_child.running() and result is GDScriptFunctionState:
+		yield(result, "completed")
+	return set_state(bt_child)
+
+
 func _ready():
 	assert(get_child_count() == 1)
