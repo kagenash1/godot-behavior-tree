da137746c3a14af55d25bd086e0fe4966574be9b
Gabriele Torini <gabriele.torini@outlook.it>
2021-03-12 15:06:15 +0100

    Update bt_decorator.gd

--- bt_decorator.gd
+++ bt_decorator.gd
@@ -5,9 +5,15 @@ extends BTNode
 # Can be used to create conditions. For example, if we extend this script we can do:
 #
 # func _tick(agent: Node, blackboard: Blackboard) -> bool:
-#    if agent.get("agent_property") and blackboard.get_data("blackboard_entry"):
-#        return ._tick(agent, blackboard)
-#    return fail()
+#	assert("agent_property" in agent)
+#
+#	if not blackboard.has_data("blackboard_property"):
+#		return fail()
+#
+#	if agent.get("agent_property") and blackboard.get_data("blackboard_entry"):
+#		return ._tick(agent, blackboard)
+#	else:
+#		return fail()
 #
 # This will execute the child only if a certain condition is met, and then match state. 
 # So you can decorate a leaf node to be executed only under certain conditions.
